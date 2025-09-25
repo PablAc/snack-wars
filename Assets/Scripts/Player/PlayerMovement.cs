8e698f246c07d7c642de8dbd59e06c6076b447c6
AgusCeb <agustincblls@gmail.com>
2025-09-24 23:50:27 -0300

    Player Animation
    Animaciones de caminata para todas las direcciones

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,21 +1,50 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D Rb2d;
-    [SerializeField] private float speed;
+    private Animator _animator;
+    [SerializeField] private float _speed;
+    private Vector2 _moveInput;
+    private Vector2 _lastMoveDir = Vector2.down;
 
 
     private void Start()
     {
         Rb2d = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
-
 
     private void OnMove(InputValue inputValue)
     {
-        Vector2 move = inputValue.Get<Vector2>();
-        Rb2d.linearVelocity = move * speed;
+        _moveInput = inputValue.Get<Vector2>();
     }
+
+    private void Update()
+    {
+        _animator.SetFloat("MoveX", _moveInput.x);
+        _animator.SetFloat("MoveY", _moveInput.y);
+        
+        
+        
+        
+        if(_moveInput.x != 0 || _moveInput.y != 0)
+        {
+
+        _animator.SetFloat("LastMoveX", _moveInput.x);
+        _animator.SetFloat("LastMoveY", _moveInput.y);
+           
+        }
+
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Rb2d.linearVelocity = _moveInput * _speed;
+    }
+
 }
